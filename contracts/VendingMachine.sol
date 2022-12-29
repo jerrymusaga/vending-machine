3a7d716b2ec698d078f80e6f3b2a06cbd568a56a
jerrymusaga <musagatechnology@gmail.com>
2022-12-29 15:50:38 +0100

    logic in smart contract

--- contracts/VendingMachine.sol
+++ contracts/VendingMachine.sol
@@ -3,6 +3,21 @@
 pragma solidity 0.8.17;
 
 contract VendingMachine {
-    
+    address public owner;
+    mapping(address => uint) public donutBalance;
+
+    constructor(){
+        owner = msg.sender;
+        donutBalance[address(this)] = 25;
+    }
+
+    modifier onlyOwner() {
+        require(owner == msg.sender, "only owner can call this method");
+    }
+
+    function getDonutBalance() public view returns (uint){
+        return donutBalance[address(this)];
+    }
+
 }
 
